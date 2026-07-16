5c44091e2ce13c29b3e875c7f7615347587cee7a
Aarti Prajapati <aartiprajapati-ai>
2026-07-16 16:48:44 +0530

    Add logout functionality

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,4 +6,13 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
+
+  res.json({
+    success: true,
+    message: "Logout Successful",
+  });
+});
+
 export default router;
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,4 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
+
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout");
+
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <nav className="h-16 bg-[#131B35] border-b border-slate-700 flex items-center justify-between px-8">
       <h1 className="text-2xl font-bold text-violet-400">
@@ -8,7 +23,10 @@ function Navbar() {
       <div className="flex items-center gap-4">
         <span className="text-white">Hi, Aarti 👋</span>
 
-        <button className="bg-red-500 px-4 py-2 rounded-lg text-white hover:bg-red-600">
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 px-4 py-2 rounded-lg text-white hover:bg-red-600"
+        >
           Logout
         </button>
       </div>
